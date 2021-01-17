d712577edcd22d015c810dc8b2323bbac6838753
GibMirRechte <55894240+DiesesForum@users.noreply.github.com>
2021-01-17 12:29:43 +0100

    Added 1.0.2
    [+] Fixed some bugs
    [+] Performance improvements

--- src/de/blockworlds/listener/WorldChangeListener.java
+++ src/de/blockworlds/listener/WorldChangeListener.java
@@ -7,6 +7,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 
+import de.blockworlds.main.BlockWorlds;
+
 public class WorldChangeListener implements Listener {
 	
 	File file = new File("plugins//BlockWorlds//worlds.yml");
@@ -14,7 +16,7 @@ public class WorldChangeListener implements Listener {
 	
 	@EventHandler
 	public void onWorldChange(PlayerChangedWorldEvent event) {
-		if(worlds.getStringList("BlockedWorlds").contains(event.getPlayer().getWorld().getName())) {
+		if(BlockWorlds.blocked_worlds.contains(event.getPlayer().getWorld().getName())) {
 			event.getPlayer().teleport(event.getFrom().getSpawnLocation());
 			event.getPlayer().sendMessage("§7[§cBlockWorlds§7] §cThis world is blocked.");
 			event.getPlayer().sendMessage("§7[§cBlockWorlds§7] §cYou have been send back!");
--- src/de/blockworlds/main/BlockWorlds.java
+++ src/de/blockworlds/main/BlockWorlds.java
@@ -2,6 +2,7 @@ package de.blockworlds.main;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
@@ -14,6 +15,8 @@ import de.blockworlds.listener.WorldChangeListener;
 
 public class BlockWorlds extends JavaPlugin {
 	
+	public static ArrayList<String> blocked_worlds = new ArrayList<>();
+	
 	File file = new File("plugins//BlockWorlds//worlds.yml");
 	YamlConfiguration worlds = YamlConfiguration.loadConfiguration(file);
 	
@@ -24,21 +27,25 @@ public class BlockWorlds extends JavaPlugin {
 			list.add("world_the_end");
 			worlds.set("BlockedWorlds",list);
 			try { worlds.save(file); } catch (IOException exception) {}
-			
-			Bukkit.getPluginManager().registerEvents(new WorldChangeListener(), this);
-			Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
-			Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
-			
-			System.out.println(" ");
-			System.out.println(" ");
-			System.out.println(" ");
-			System.out.println("========== BlockWorlds ==========");
-			System.out.println(" ");
-			System.out.println("Author: GibMirRechte");
-			System.out.println("Version: " + Bukkit.getPluginManager().getPlugin("BlockWorlds").getDescription().getVersion());
-			System.out.println("Discord: yhBX6KT");
-			System.out.println(" ");
-			System.out.println("========== BlockWorlds ==========");
 		}
+		
+		for(String w : worlds.getStringList("BlockedWorlds")) {
+			blocked_worlds.add(w);
+		}
+		
+		Bukkit.getPluginManager().registerEvents(new WorldChangeListener(), this);
+		Bukkit.getPluginManager().registerEvents(new ChatListener(), this);
+		Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
+		
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("========== BlockWorlds ==========");
+		System.out.println(" ");
+		System.out.println("Author: GibMirRechte");
+		System.out.println("Version: " + Bukkit.getPluginManager().getPlugin("BlockWorlds").getDescription().getVersion());
+		System.out.println("Discord: yhBX6KT");
+		System.out.println(" ");
+		System.out.println("========== BlockWorlds ==========");
 	}
 }
